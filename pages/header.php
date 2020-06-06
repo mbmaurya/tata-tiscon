2a140471aa4e51bc59114320699731552fd49ae9
PankajRane <pankaj@teampumpkin.com>
2020-06-06 18:37:56 +0530

    Added Footer Files

--- pages/header.php
+++ pages/header.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="./css/bootstrap-reboot.min">
+    <link rel="stylesheet" href="./css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="./css/bootstrap-grid.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/button.css">
     <link rel="stylesheet" href="./css/form.css">
-
+    <link rel="stylesheet" href="./css/typography.css">
+    <link rel="stylesheet" href="./css/footer.css">
 
     <title>Hello, world!</title>
   </head>
