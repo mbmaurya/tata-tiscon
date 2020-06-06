2ee449a92651abaa7f60e6258c2661a6583c3c2d
Mukesh Maurya <mukesh@insider.in>
2020-06-06 19:30:59 +0530

    create navbar component  and merge changes from pull

--- pages/header.php
+++ pages/header.php
@@ -6,14 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="./css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="./css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/button.css">
     <link rel="stylesheet" href="./css/form.css">
     <link rel="stylesheet" href="./css/typography.css">
     <link rel="stylesheet" href="./css/footer.css">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="./css/navbar.css">
+    <title>Tata Tiscon</title>
   </head>
   <body>
+    <?php include 'pages/navbar.php';?>
