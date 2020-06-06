ae12c3d555735424e9105baf536d1a6a00a9a640
Mukesh Maurya <mukesh@insider.in>
2020-06-06 12:31:02 +0530

    setup folder structure

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php include 'pages/header.php';?>
+
+    <h1>Hello, world!</h1>
+    
+<?php include 'pages/footer.php';?>
+
+    
---
+++ pages/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./css/bootstrap-reboot.min">
+    <link rel="stylesheet" href="./css/bootstrap-grid.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
