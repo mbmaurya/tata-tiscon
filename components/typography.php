be54d397cf15624ca349ef1d677abec410d6301d
PankajRane <pankaj@teampumpkin.com>
2020-06-06 16:05:25 +0530

    typography

---
+++ components/typography.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="../css/typography.css">
+</head>
+<body>
+  <h1 class="text-black"> The quick brown fox jumps over the lazy dog </h1>
+  <h2 class="text-blue text-uppercase"> The quick brown fox jumps over the lazy dog </h2>
+  <h3 class="text-red"> The quick brown fox jumps over the lazy dog </h3>
+  <h4> The quick brown fox jumps over the lazy dog </h4>
+  <p class="header-txt"> The quick brown fox jumps over the lazy dog </p>
+  <p class="footer-txt"> The quick brown fox jumps over the lazy dog </p>
+  <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+</body>
+</html>
