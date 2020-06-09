1917881d746f06c968dbf3a20da5839e2ec5b6d9
ShubhamIngale <32031146+ShubhamIngale@users.noreply.github.com>
2020-06-10 01:52:45 +0530

    header, footer refined and mason page ask done

---
+++ navbar.php
@@ -0,0 +1,67 @@
+<nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-lg-4 v-center nav-left">
+                <img src="images/tata-tiscon-logo.png" alt="Tata Tiscon Logo" class="img-fluid logo" />
+                <img src="images/quote.svg" class="img-fluid quote" />
+            </div>
+            <div class="col-lg-5 v-center nav-center">
+                <ul class="menu">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Products</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Programs</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Building Guide</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-lg-3 v-center nav-right">
+                <button class="btn buy-online">
+                    <img src="" />
+                    Buy Online
+                </button>
+                <a href="#" class="search-btn">
+                    <img src="images/search.svg" />
+                </a>
+
+                <div class="hamburger-menu">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><img src="images/breadcrumb.svg"/></a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
