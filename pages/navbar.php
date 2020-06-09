4455713373caf6202357495739d7a30abe44e606
Shubham Ingale <shubhamingale@Shubhams-MacBook-Pro.local>
2020-06-10 00:52:21 +0530

    header, footer, mason page refined and mason page task complete

--- pages/navbar.php
+++ pages/navbar.php
@@ -5,7 +5,7 @@
                 <img src="images/tata-tiscon-logo.png" alt="Tata Tiscon Logo" class="img-fluid logo" />
                 <img src="images/quote.svg" class="img-fluid quote" />
             </div>
-            <div class="col-lg-5 v-center nav-center">
+            <div class="col-lg-5 v-center nav-center d-xs-none">
                 <ul class="menu">
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Products</a>
@@ -51,7 +51,7 @@
                     <img src="images/search.svg" />
                 </a>
 
-                <div class="hamburger-menu">
+                <div class="hamburger-menu desktop">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><img src="images/breadcrumb.svg"/></a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">Action</a>
@@ -62,6 +62,59 @@
                     </div>
                 </div>
             </div>
+            <!-- hamburger for mobile -->
+            <div class="hamburger-menu mobile">
+                    <a class="navbar-toggler" data-toggle="collapse" href="#" role="button" data-target="#mobileMenu" aria-controls="mobileMenu" aria-expanded="false" aria-label="Toggle navigation"><img src="images/breadcrumb.svg"/></a>
+                    <div class="collapse navbar-collapse" id="mobileMenu">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Products</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Separated link</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Separated link</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Programs</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Separated link</a>
+                                </div>
+                            </li>
+                            <li class=" nav-item dropdown mt-2">
+                                <a href="#">Building Guide</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
         </div>
+
+        <!-- search for mobile -->
+        <div class="row mt-2">
+            <div class="col-sm-12 col-12 mobile-search">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="What are ou looking for?" aria-label="What are you looking for?">
+                <button class="btn my-2 my-sm-0" type="submit"><img src="./images/mason/search.svg" alt="src"></button>
+            </form>
+            </div>
+        </div>
+        
     </div>
 </nav>
